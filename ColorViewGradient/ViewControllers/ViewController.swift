84dd12cf63959818e27bffe0b47d8b0f32731431
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-03 18:53:34 +0500

    - Разместил объекты на View - Установил Constrats

---
+++ ColorViewGradient/ViewControllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ColorViewGradient
+//
+//  Created by Dmitry Parhomenko on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    
+    // MARK: - Pablic Properties
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Initialisers
+    
+    // MARK: - Ovverides Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IB Actions
+    
+    // MARK: - Pablic Methods
+    
+    // MARK: - Private Methods
+   
+}
+
