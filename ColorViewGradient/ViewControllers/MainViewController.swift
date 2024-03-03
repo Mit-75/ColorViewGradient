d5fc0ea82ef597226641d540d418feac8d83bf23
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-03 21:10:22 +0500

    - Добавил developer branch

--- ColorViewGradient/ViewControllers/MainViewController.swift
+++ ColorViewGradient/ViewControllers/MainViewController.swift
@@ -13,9 +13,18 @@ class MainViewController: UIViewController {
     
     @IBOutlet var mainColorView: UIView!
     
-    @IBOutlet var colorsView: [UIView]!
-    @IBOutlet var valueSlayders: [UILabel]!
-    @IBOutlet var sliders: [UISlider]!
+   
+    @IBOutlet var redView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var blueView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     // MARK: - Pablic Properties
     
@@ -24,13 +33,48 @@ class MainViewController: UIViewController {
     // MARK: - Ovverides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        getSetupUI()
+        getViewGradient()
     }
     
     // MARK: - IB Actions
     
-    // MARK: - Pablic Methods
-    
-    // MARK: - Private Methods
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        getViewGradient()
+        
+    }
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        getViewGradient()
+       
+    }
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        getViewGradient()
+       
+    }
    
+}
+extension MainViewController {
+    
+    private func getSetupUI() {
+        redSlider.value = 0.1
+        greenSlider.value = 0.5
+        blueSlider.value = 0.9
+        redValueLabel.text = String(redSlider.value)
+        greenValueLabel.text = String(greenSlider.value)
+        blueValueLabel.text = String(blueSlider.value)
+    }
+    
+    private func getViewGradient() {
+        mainColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+    }
+    
 }
 
