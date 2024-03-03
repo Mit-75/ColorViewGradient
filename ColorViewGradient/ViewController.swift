031b48624c26bf0a0267c230fcc99a99af5bb804
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-03 17:57:00 +0500

    Initial Commit

---
+++ ColorViewGradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorViewGradient
+//
+//  Created by Dmitry Parhomenko on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
